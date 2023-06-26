f272c9a0fc86f37c6e5ab5f870bc95b457aa3f50
augusto <augustosaaude@gmail.com>
2023-06-26 16:44:45 -0300

    finish comments

--- projeto/box-game.py
+++ projeto/box-game.py
@@ -1,79 +1,21 @@
 import csv
 from algoritmoDPLL import *
 
-
-def abrirTabelaCNF(CNF):
-    with open(CNF) as arquivo:
-        grid = []
-        
-        for linha in arquivo:
-            if linha[0] != 'c' and linha[0] != 'p' and linha[0] != '%' and linha[0] != '0' and linha[0] != ' ':
-                linhaInt = []
-                
-                linha = linha.split(' ')
-                linha.pop(-1)
-                
-                for elemento in linha:
-                    linhaInt.append(int(elemento))
-                
-                grid.append(linhaInt)
-
-    return grid
-
-
-def criarClausulas(arquivo):
-    with open(arquivo) as csv_file:
-        grid = []
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        
-        for linha in csv_reader:
-            linhaCnf = []
-            for atomica in linha:
-                linhaCnf.append(atomica.replace(' ', ''))
-            grid.append(linhaCnf)
-
-        grid.pop(0)
-    
-        return grid
-
-def solucao(arquivo):
-    formulaDIMACS, numeroAtomicas, listaAtomicasNumeros = transformarEmDIMACS(arquivo)
-    criarArquivosDIMACS(formulaDIMACS, numeroAtomicas)
-    
-    arquivoDIMACS = abrirTabelaCNF('BoxGameCNF/dados-boxgame.txt')
-    resultado = DPLL(arquivoDIMACS)
-
-    print(resultado)
-    print(listaAtomicasNumeros)
-
-    for removidos in listaAtomicasNumeros:
-        test = removidos[0] in resultado
-        
-        if test == False:
-            print(removidos[0])
-
-
-def transformarEmAtomicas(resultado, listaAtomicasNumeros):
-    resultadoAtomicas = []
-    
-    for elementos in resultado:
-        for posicao in listaAtomicasNumeros:
-            if elementos == posicao[0]:
-                resultadoAtomicas.append(str(posicao[1]))
-                
-    return resultadoAtomicas
-
-
 def transformarEmDIMACS(formulaCNF):
-    listaDeClausulas = []
+    # armazena as cláusulas da formulaCNF EX: ['q1', 'q2', 'q3]
+    listaDeClausulas = [] 
+    # armazena a lista de numero de atômicas
+    # EX: [[1, 'q1'], [2, 'q2'], [3, 'q3'], [4, 'q4'], [5, 'r1'], [6, 'r2']
     listaNumerosEAtomicas = []
+    # armazena a formulaDIMACS em valores inteiros transformada a partir do arquivo
     formulaDIMACS = []
+    # armazena os numeros de atômicas da formulaCFN
     numeroAtomicas = 0
 
     for clausula in formulaCNF:
         for literal in clausula:
             listaDeClausulas.append(literal)
-                
+    
     for index, clausula in enumerate(sorted(set(listaDeClausulas))):
         listaNumerosEAtomicas.append([index + 1, clausula])
         numeroAtomicas += 1 
@@ -86,11 +28,17 @@ def transformarEmDIMACS(formulaCNF):
                     listaAuxiliar.append(teste[0])
                     
         formulaDIMACS.append(listaAuxiliar)
+    
+    print('FormulaDIMACS:',formulaDIMACS)
+    print('NumeroAtomicas:', numeroAtomicas)
+    print('ListaNumerosEAtomicas:',listaNumerosEAtomicas)
 
     return formulaDIMACS, numeroAtomicas, listaNumerosEAtomicas
 
 
 def criarArquivosDIMACS(formulaDIMACS, numeroAtomicas):
+    # armazena o numero de clausulas que vai ser usado para
+    # colocar no arquivo de dados-boxgame.txt
     numeroClausulas = 0
     
     for clausula in formulaDIMACS:
@@ -108,4 +56,38 @@ def criarArquivosDIMACS(formulaDIMACS, numeroAtomicas):
     return
 
 
-solucao(criarClausulas('BoxGameCNF/boxgame.txt'))
+# essa função vai pegar todos os dados de boxgame.txt e colocar dentro de uma 
+# lista, removendo a primeira linha, ou seja, vai pegar somente as cláusulas.
+def criarClausulas(arquivo):
+    with open(arquivo) as csv_file:
+        grid = []
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        
+        for linha in csv_reader:
+            linhaCnf = []
+            for atomica in linha:
+                linhaCnf.append(atomica.replace(' ', ''))
+            grid.append(linhaCnf)
+
+        grid.pop(0)
+        return grid
+
+
+def solucao(arquivo):
+    formulaDIMACS, numeroAtomicas, listaAtomicasNumeros = transformarEmDIMACS(arquivo)
+
+    criarArquivosDIMACS(formulaDIMACS, numeroAtomicas)
+    
+    resultado = DPLL(formulaDIMACS)
+
+    resultRemovidos = []
+
+    for removidos in listaAtomicasNumeros:
+        test = removidos[0] in resultado
+
+        if test == False:
+            resultRemovidos.append(removidos[0])
+
+    return resultRemovidos
+
+print(solucao(criarClausulas('BoxGameCNF/boxgame.txt')))
